f9a1304a0a3eca697a9448e6c8dc03c0c40b923f
EricMChavez <eric@emcaudio.com>
2019-03-25 21:18:26 -0500

    completed

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let toDo = [];
+let completed = [];
 function add() {
 	let item = document.querySelector('#input-field').value;
 	document.querySelector('#input-field').value = '';
@@ -10,12 +11,34 @@ function addEntry() {
 	for (i in toDo) {
 		var iDiv = document.createElement('div');
 		iDiv.className = 'card m-3 pb-5 pt-2 col-3';
-		iDiv.innerHTML = toDo[i] + '<button class="X" onclick="remove(' + i + ')">X</button>';
+		iDiv.id = 'card' + i;
+		iDiv.innerHTML =
+			toDo[i] +
+			'<button class="X" onclick="remove(' +
+			i +
+			')">X</button>' +
+			'<button class="complete" onclick="complete(' +
+			i +
+			')">✓</button>';
 		document.querySelector('#entries').appendChild(iDiv);
 	}
 }
-function remove(itemNumber) {
-	toDo.splice(itemNumber, 1);
+function complete(taskNumber) {
+	var completeditem = document.getElementById('card' + taskNumber);
+	var iDiv = document.createElement('div');
+	iDiv.className = 'card bg-success m-3 pb-5 pt-2 col-2';
+	iDiv.id = 'card' + i;
+	iDiv.innerHTML = toDo[i];
+	document.querySelector('#completed').appendChild(iDiv);
+	remove(taskNumber);
+}
+function remove(taskNumber) {
+	toDo.splice(taskNumber, 1);
+	clearList();
+	addEntry();
+}
+function removeCompleted(taskNumber) {
+	completed.splice(taskNumber, 1);
 	clearList();
 	addEntry();
 }
