543176fdd7c1252e48ca75b07163bdccdb15751e
EricMChavez <eric@emcaudio.com>
2019-03-25 20:47:13 -0500

    makin' purdy

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 let toDo = [];
 function add() {
 	let item = document.querySelector('#input-field').value;
-	toDo.push(item);
-	console.log(toDo);
+	document.querySelector('#input-field').value = '';
+	toDo.unshift(item);
 	clearList();
 	addEntry();
 }
 function addEntry() {
 	for (i in toDo) {
 		var iDiv = document.createElement('div');
-		iDiv.className = 'd-block';
+		iDiv.className = 'card m-3 pb-5 pt-2 col-3';
 		iDiv.innerHTML = toDo[i] + '<button class="X" onclick="remove(' + i + ')">X</button>';
 		document.querySelector('#entries').appendChild(iDiv);
 	}
