f349000e644bd9d394a985b82f107ae2591dbc14
EricMChavez <eric@emcaudio.com>
2019-03-25 20:20:10 -0500

    working

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
 let toDo = [];
 function add() {
 	let item = document.querySelector('#input-field').value;
+	toDo.push(item);
+	console.log(toDo);
+	clearList();
+	addEntry();
+}
+function addEntry() {
+	for (i in toDo) {
+		var iDiv = document.createElement('div');
+		iDiv.className = 'd-block';
+		iDiv.innerHTML = toDo[i] + '<button class="X" onclick="remove(' + i + ')">X</button>';
+		document.querySelector('#entries').appendChild(iDiv);
+	}
+}
+function remove(itemNumber) {
+	toDo.splice(itemNumber, 1);
+	clearList();
+	addEntry();
+}
+
+function clearList() {
+	document.querySelector('#entries').innerHTML = '';
 }
