d8760498ef99c5d408f9b38a597e0139fb1d851b
EricMChavez <eric@emcaudio.com>
2019-03-26 21:44:50 -0500

    animated

--- index.js
+++ index.js
@@ -1,54 +1,87 @@
 let toDo = [];
 let completed = [];
+
 function add() {
 	let task = document.querySelector('#input-field').value;
 	document.querySelector('#input-field').value = '';
-	toDo.unshift(task);
-	clearList();
-	readList();
+	toDo.push(task);
+	clearToDo();
+	readToDo(true);
 }
-function readList() {
+function readToDo(newItem) {
 	for (i in toDo) {
-		var iDiv = document.createElement('div');
-		iDiv.className = 'card text-center rounded-top m-3 pb-5 pt-1 col-2';
-		iDiv.id = 'card' + i;
-		iDiv.innerHTML =
-			toDo[i] +
-			'<button class="X" onclick="remove(' +
-			i +
-			')">X</button>' +
-			'<button class="complete" onclick="complete(' +
-			i +
-			')">✓</button>';
-		document.querySelector('#entries').appendChild(iDiv);
+		if (i == toDo.length - 1 && newItem == true) {
+			var iDiv = document.createElement('div');
+			iDiv.className = 'card text-center animated bounceInRight rounded-top m-3 pb-5 pt-1 col-2';
+			iDiv.id = 'card' + i;
+			iDiv.innerHTML =
+				toDo[i] +
+				'<button class="X" onclick="remove(' +
+				i +
+				')">X</button>' +
+				'<button class="complete" onclick="complete(' +
+				i +
+				')">✓</button>';
+			document.querySelector('#entries').appendChild(iDiv);
+		} else {
+			var iDiv = document.createElement('div');
+			iDiv.className = 'card text-center rounded-top m-3 pb-5 pt-1 col-2';
+			iDiv.id = 'card' + i;
+			iDiv.innerHTML =
+				toDo[i] +
+				'<button class="X" onclick="remove(' +
+				i +
+				')">X</button>' +
+				'<button class="complete" onclick="complete(' +
+				i +
+				')">✓</button>';
+			document.querySelector('#entries').appendChild(iDiv);
+		}
 	}
+}
+function readCompleted(newItem) {
 	for (i in completed) {
-		var xDiv = document.createElement('div');
-		xDiv.className = 'card bg-success text-center border-warning m-3 pb-5 pt-2 col-2';
-		xDiv.id = 'card' + i;
-		xDiv.innerHTML = completed[i] + '<button class="completedX" onclick="removeCompleted(' + i + ')">X</button>';
-		document.querySelector('#completed').appendChild(xDiv);
+		if (i == completed.length - 1 && newItem == true) {
+			var xDiv = document.createElement('div');
+			xDiv.className = 'card bg-success text-center animated bounceInRight border-warning m-3 pb-5 pt-2 col-2';
+			xDiv.id = 'card' + i;
+			xDiv.innerHTML =
+				completed[i] + '<button class="completedX" onclick="removeCompleted(' + i + ')">X</button>';
+			document.querySelector('#completed').appendChild(xDiv);
+		} else {
+			var xDiv = document.createElement('div');
+			xDiv.className = 'card bg-success text-center border-warning m-3 pb-5 pt-2 col-2';
+			xDiv.id = 'card' + i;
+			xDiv.innerHTML =
+				completed[i] + '<button class="completedX" onclick="removeCompleted(' + i + ')">X</button>';
+			document.querySelector('#completed').appendChild(xDiv);
+		}
 	}
 }
 function complete(taskNumber) {
-	completed.unshift(toDo[taskNumber]);
+	completed.push(toDo[taskNumber]);
 	toDo.splice(taskNumber, 1);
-	clearList();
-	readList();
+	clearCompleted();
+	clearToDo();
+	readToDo();
+	readCompleted(true);
 }
 function remove(taskNumber) {
 	toDo.splice(taskNumber, 1);
-	clearList();
-	readList();
+	clearToDo();
+	clearToDo();
+	readToDo();
 }
 function removeCompleted(taskNumber) {
 	completed.splice(taskNumber, 1);
-	clearList();
-	readList();
+	clearCompleted();
+	readCompleted();
 }
 
-function clearList() {
+function clearToDo() {
 	document.querySelector('#entries').innerHTML = '';
+}
+function clearCompleted() {
 	document.querySelector('#completed').innerHTML = '';
 }
 var input = document.getElementById('input-field');
